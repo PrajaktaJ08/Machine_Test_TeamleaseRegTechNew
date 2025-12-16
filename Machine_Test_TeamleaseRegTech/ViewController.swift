f04ac1099ba914406c0d7b577513edd981bf688c
Prajakta Jadhav <jadhavprajkta08@gmail.com>
2025-12-16 19:18:18 +0530

    Initial Commit

---
+++ Machine_Test_TeamleaseRegTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Machine_Test_TeamleaseRegTech
+//
+//  Created by Prajakta Prakash Jadhav on 16/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
